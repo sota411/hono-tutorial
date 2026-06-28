95e6630811bc04d9aa5cdd3ab84a55ab4cc27710
sota411 <soutapon0119@gmail.com>
2026-06-29 03:43:55 +0900

    add: /admin エンドポイントと基本認証を追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
 
 const app = new Hono()
+
+app.use(
+  '/admin/*',
+  basicAuth({
+    username: 'admin',
+    password: 'secret',
+  })
+)
+
+app.get('admin', (c) => {
+  return c.text('You are authenticated!')
+})
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
